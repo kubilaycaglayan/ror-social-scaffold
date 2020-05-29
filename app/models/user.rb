33b73d29853a27e87632c25f115b08f953b4148b
mariosknl <mariosknl@outlook.com>
2020-05-29 16:53:35 +0300

    Invitations method created

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,15 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_many :invitations, class_name: 'Friendship', foreign_key: 'sender_id'
+  has_many :pending_invitations, class_name: 'Friendship', foreign_key: 'receiver_id'
+
+  def send_invitation(receiver)
+    invitations.create(receiver_id: receiver.id)
+  end
+
+  def destroy_pending_invitation(sender)
+    pending_invitation = pending_invitations.where(sender_id: sender.id)
+    pending_invitation.destroy(pending_invitation.ids[0])
+  end
 end
