57b181d572f15899a871d2d4252ad71109abbee6
Kubilay <kblycaglayan@gmail.com>
2020-06-02 17:45:35 +0300

    Test for two row model

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -32,4 +32,13 @@ RSpec.describe Friendship, type: :model do
       expect(friendship).not_to be_valid
     end
   end
+
+  describe 'has two row approach' do
+    it 'creates a mirror row when invitation is accepted' do
+      Friendship.create(user_id: 2, friend_id: 1)
+      User.first.accept_pending_invitation(User.last)
+      record = Friendship.where(user_id: User.first.id, friend_id: User.last.id, status: true)
+      expect(record.empty?).not_to be true
+    end
+  end
 end
